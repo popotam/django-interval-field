ff6e9f97a395cb5bdb97db4e4da57b9dbbd59342
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2014-11-04 19:14:13 +0100

    Fix comparison of relativedeltas (min/max value)
    Relativedeltas don't support comparison on their own.

--- interval/forms.py
+++ interval/forms.py
@@ -190,12 +190,12 @@ class IntervalFormField(Field):
 
         if cleaned_value is not None:
             if self.min_value is not None:
-                if cleaned_value < self.min_value:
+                if self._cmp_relativedeltas(cleaned_value, self.min_value) == -1:
                     raise ValidationError(
                     _("This interval must be at least: %s") % self.min_value)
 
             if self.max_value is not None:
-                if cleaned_value > self.max_value:
+                if self._cmp_relativedeltas(cleaned_value, self.max_value) == 1:
                     raise ValidationError(
                         _("This interval must be no more than: %s"
                           ) % self.max_value)
@@ -205,3 +205,10 @@ class IntervalFormField(Field):
                 raise ValidationError(self.default_error_messages['required'])
 
         return Field.clean(self, cleaned_value)
+
+    @staticmethod
+    def _cmp_relativedeltas(lhs, rhs):
+        return cmp(
+            (lhs.years, lhs.months, lhs.days, lhs.hours, lhs.minutes, lhs.seconds, lhs.microseconds),
+            (rhs.years, rhs.months, rhs.days, rhs.hours, rhs.minutes, rhs.seconds, rhs.microseconds),
+        )
